6ef70c6a572a3f3117580df0e880d6726734f1c7
ankiit-ee <56360028+ankiit-ee@users.noreply.github.com>
2020-06-04 01:59:31 +0530

    Add files via upload

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css'],
+})
+export class SidenavListComponent implements OnInit {
+  @Output() closesidenav = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  close() {
+    this.closesidenav.emit();
+  }
+}
